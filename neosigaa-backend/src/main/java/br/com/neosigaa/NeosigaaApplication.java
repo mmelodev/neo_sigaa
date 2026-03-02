20d75bca30053dd2f6075c3dd7028b4928432403
Murilo Melo <murilomelomd@gmail.com>
2026-03-02 15:59:48 -0300

    começando estrutura das pastas

---
+++ neosigaa-backend/src/main/java/br/com/neosigaa/NeosigaaApplication.java
@@ -0,0 +1,13 @@
+package br.com.neosigaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeosigaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeosigaaApplication.class, args);
+	}
+
+}
---
+++ neosigaa-backend/src/test/java/br/com/neosigaa/NeosigaaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.neosigaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeosigaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
