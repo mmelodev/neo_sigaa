947d19b1a319bd589901ae51e7de1bee1bdd8e28
Murilo Melo <murilomelomd@gmail.com>
2026-03-04 15:59:44 -0300

    simbora playwright

--- neosigaa-backend/src/main/java/br/com/neosigaa/NeosigaaApplication.java
+++ neosigaa-backend/src/main/java/br/com/neosigaa/NeosigaaApplication.java
@@ -1,13 +1,24 @@
 package br.com.neosigaa;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @SpringBootApplication
 public class NeosigaaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeosigaaApplication.class, args);
+		//simbora playwright
 	}
 
 }
